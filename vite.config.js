9f7f8f895f6d12a9d7c0d2a827ac01b803eb837e
Антон Михайлов <antonmih006@gmail.com>
2025-01-25 17:51:57 +0300

    change config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
+
+export default defineConfig({
+    root: 'solution',
+    build: {
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'solution/index.html'),
+                test: resolve(__dirname, 'solution/test/index.html'),
+            },
+        },
+    },
+});
